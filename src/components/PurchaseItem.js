e04732deab1e4e62d7de3921901a5a55df375c12
n0ahsun <34866479+n0ahsun@users.noreply.github.com>
2023-01-19 21:31:44 +0800

    added date varibales

--- src/components/PurchaseItem.js
+++ src/components/PurchaseItem.js
@@ -1,9 +1,17 @@
 import "./PurchaseItem.css";
 
 function PurchaseItem(props) {
+  const year = props.date.getFullYear();
+  const month = props.date.toLocaleString('en-US', {month:'long'});
+  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+
   return (
     <div className="purchase-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <h2 className="purchase-item-description">{props.title}</h2>
       <div className="purchase-item-price">${props.amount}</div>
     </div>
