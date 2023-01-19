a80216ab716bf3f4d50a0da15480bbf1774219a2
n0ahsun <34866479+n0ahsun@users.noreply.github.com>
2023-01-19 15:39:26 +0800

    added purchaseitem component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import PurchaseItem from "./components/PurchaseItem";
+
+
 function App() {
   return (
     <div>
       <h1>React App</h1>
+      <PurchaseItem></PurchaseItem>
     </div>
   );
 }
---
+++ src/components/PurchaseItem.js
@@ -0,0 +1,13 @@
+import "./PurchaseItem.css";
+
+function PurchaseItem() {
+  return (
+    <div className="purchase-item">
+      <div>Jan 20th 2023</div>
+      <h2 className="purchase-item-description">Food</h2>
+      <div className="purchase-item-price">$12.34</div>
+    </div>
+  );
+}
+
+export default PurchaseItem;
