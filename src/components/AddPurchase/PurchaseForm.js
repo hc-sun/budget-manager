cb8ac5d301c7f4aba86843257ba175c4077d5d34
n0ahsun <34866479+n0ahsun@users.noreply.github.com>
2023-01-22 16:38:27 +0800

    added state update for form

--- src/components/AddPurchase/PurchaseForm.js
+++ src/components/AddPurchase/PurchaseForm.js
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PurchaseForm.css";
 
 function PurchaseForm() {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+
+  function titleChange(event) {
+    setTitle(event.target.value);
+  }
+
+  function amountChange(event) {
+    setAmount(event.target.value);
+  }
+
+  function dateChange(event) {
+    setDate(event.target.value);
+  }
+
   return (
     <form>
       <div className="add-purchase-controls">
         <div className="add-purchase-control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChange} />
         </div>
         <div className="add-purchase-control">
           <label>Amount</label>
-          <input type="number" />
+          <input type="number" onChange={amountChange} />
         </div>
         <div className="add-purchase-control">
           <label>Date</label>
-          <input type="date" min="2020-01-01" max="2025-12-31" />
+          <input
+            type="date"
+            min="2020-01-01"
+            max="2025-12-31"
+            onChange={dateChange}
+          />
         </div>
       </div>
       <div className="add-purchase-actions">
-        <button type='submit'>Add Purchase</button>
+        <button type="submit">Add Purchase</button>
       </div>
     </form>
   );
