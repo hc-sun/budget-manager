da12475011bb644a4cfc67406e13bb03b2823c4d
n0ahsun <34866479+n0ahsun@users.noreply.github.com>
2023-01-22 20:16:00 +0800

    removed change title button

--- src/components/Purchases/PurchaseItem.js
+++ src/components/Purchases/PurchaseItem.js
@@ -1,25 +1,26 @@
 import "./PurchaseItem.css";
 import PurchaseDate from "./PurchaseDate";
 import Card from "../UI/Card";
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
 
 function PurchaseItem(props) {
 
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    {/*use setTitle to assign new value*/}
-    setTitle('Updated');
-  };
+  // const clickHandler = () => {
+  //   {/*use setTitle to assign new value*/}
+  //   setTitle('Updated');
+  // };
 
   return (
     <Card className="purchase-item">
       <PurchaseDate date={props.date} />
       <div className="purchase-item-description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
+        {/* <h2>{title}</h2> */}
         <div className="purchase-item-price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change</button>
+      {/* <button onClick={clickHandler}>Change</button> */}
     </Card>
   );
 }
