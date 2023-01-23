31c1ce2fdc53081f7618b23d2210acebb5aaac60
n0ahsun <34866479+n0ahsun@users.noreply.github.com>
2023-01-23 16:51:15 +0800

    use map to create purchaseitem

--- src/components/Purchases/Purchases.js
+++ src/components/Purchases/Purchases.js
@@ -2,10 +2,10 @@ import PurchaseItem from "./PurchaseItem";
 import "./Purchases.css";
 import Card from "../UI/Card";
 import PurchaseFilter from "./Filter.js";
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 function Purchases(props) {
-  const [yearData, setYear] = useState('2023');
+  const [yearData, setYear] = useState("2023");
 
   function selectYearHandler(year) {
     setYear(year);
@@ -14,27 +14,17 @@ function Purchases(props) {
   return (
     <div>
       <Card className="purchases">
-        <PurchaseFilter selectedYearData={yearData} selectYear={selectYearHandler} />
-        <PurchaseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+        <PurchaseFilter
+          selectedYearData={yearData}
+          selectYear={selectYearHandler}
         />
-        <PurchaseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <PurchaseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <PurchaseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((purchase) => (
+          <PurchaseItem
+            title={purchase.title}
+            amount={purchase.amount}
+            date={purchase.date}
+          />
+        ))}
       </Card>
     </div>
   );
